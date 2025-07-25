f31e2c1c704e8b8ef07707bacb0e5f519a7f179d
MIM-BOT-KABBO <lkkabbo42@gmail.com>
2025-07-25 17:23:09 +0600

    Update hug.js

--- scripts/commands/hug.js
+++ scripts/commands/hug.js
@@ -3,7 +3,7 @@ module.exports.config = {
   version: "2.0.0",
   permission: 0,
   prefix: true,
-  credits: "kabbo",
+  credits: "kabb",
   description: "Sends a hug image",
   category: "img",
   usage: "[@mention]",
